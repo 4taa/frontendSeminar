d344aa23ba340bc34fce2ed934afcded6dfc8285
4taa <heartaccount@rambler.ru>
2019-02-12 00:11:03 +0300

    tested WeStorm

--- lecture1/index.js
+++ lecture1/index.js
@@ -1,15 +1,15 @@
-const fs = require('fs'),
-	  http = require('http'),
+const http = require('http'),
 	  express = require('express'),
 	  app = express();
 
 app.use(express.urlencoded());
 
 http.createServer(app).listen(3000);
-console.log("start");
+console.log('Server started at ' + 'http://127.0.0.1:3000');
 app.get("/", function (req, res) {
 	console.log("/index");
-	res.send("<a href=" + "index.html" + ">Start Page</a>");
+	res.send("<a href=" + "index.html" + ">Start Page</a>"
+			+ "<a href=" + "about.html" + ">Start Page</a>");
 });
 
 app.use(express.static(__dirname + "/client"));
